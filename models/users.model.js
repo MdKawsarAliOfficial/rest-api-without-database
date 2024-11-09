2caede9c08c012e1b4a38719d4591fe004a792a4
kawsar-ali <kausarrabbi023@gmail.com>
2024-11-09 21:36:47 +0600

    first commit

---
+++ models/users.model.js
@@ -0,0 +1,81 @@
+const { v4: uuidv4 } = require('uuid');
+
+const users = [
+    {
+        id: uuidv4(),
+        username: 'Alice',
+        email: 'alice@example.com',
+    },
+    {
+    id: uuidv4(),
+    username: 'Bob',
+    email: 'bob@example.com',
+    },
+    {
+    id: uuidv4(),
+    username: 'Charlie',
+    email: 'charlie@example.com',
+    },
+    {
+    id: uuidv4(),
+    username: 'David',
+    email: 'david@example.com',
+    },
+    {
+    id: uuidv4(),
+    username: 'Emily',
+    email: 'emily@example.com',
+    },
+    {
+    id: uuidv4(),
+    username: 'Frank',
+    email: 'frank@example.com',
+    },
+    {
+    id: uuidv4(),
+    username: 'Grace',
+    email: 'grace@example.com',
+    },
+    {
+    id: uuidv4(),
+    username: 'Henry',
+    email: 'henry@example.com',
+    },
+    {
+    id: uuidv4(),
+    username: 'Isabella',
+    email: 'isabella@example.com',
+    },
+    {
+    id: uuidv4(),
+    username: 'Jack',
+    email: 'jack@example.com',
+    },
+    {
+    id: uuidv4(),
+    username: 'Karen',
+    email: 'karen@example.com',
+    },
+    {
+    id: uuidv4(),
+    username: 'Liam',
+    email: 'liam@example.com',
+    },
+    {
+    id: uuidv4(),
+    username: 'Mia',
+    email: 'mia@example.com',
+    },
+    {
+    id: uuidv4(),
+    username: 'Noah',
+    email: 'noah@example.com',
+    },
+    {
+    id: uuidv4(),
+    username: 'Olivia',
+    email: 'olivia@example.com',
+    },
+];
+
+module.exports = users;
